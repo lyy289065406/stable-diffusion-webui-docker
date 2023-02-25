9a736ec44319bae99b6c5dd84b19d9c030b2170c
exp.liao <289065406@qq.com>
2023-02-25 18:08:25 +0800

    add mac

---
+++ bin/build.sh
@@ -0,0 +1,6 @@
+docker compose --profile download up --build
+
+# docker compose --profile [ui] up --build
+# where [ui] is one of: invoke | auto | auto-cpu | sygil | sygil-sl
+
+docker compose --profile auto up --build
